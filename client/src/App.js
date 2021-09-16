c9fec00921e295b17faa2385a416316d1ab8385d
Annmarie Switzer <aargiros@gmail.com>
2021-09-15 23:23:36 -0400

    wip auth

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,12 @@ function App() {
         </button>
     ));
 
+    const getData = async () => await (await fetch('http://localhost:5000/get', {
+        credentials: 'include'
+    })).json();
+
     useEffect(() => {
+        console.log(getData());
         localStorage.setItem('theme', theme);
         document.documentElement.setAttribute('data-theme', theme);
     });
@@ -29,7 +34,7 @@ function App() {
 
             <Clock date={new Date()} />
 
-            <a href="https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=604221561004-1c4rk34opibu8f52qrgmob6t30794e53.apps.googleusercontent.com&scope=openid%20email%20profile&redirect_uri=http%3A//localhost:5000/callback&state=changeme&nonce=changemetoo">
+            <a href="https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=604221561004-1c4rk34opibu8f52qrgmob6t30794e53.apps.googleusercontent.com&scope=openid%20email%20profile&redirect_uri=http%3A//localhost:5000/callback&state=changeme&nonce=changemetoo&prompt=select_account">
                 Login with Google
             </a>
         </div>
--- server.js
+++ server.js
@@ -2,28 +2,46 @@ import express from 'express';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 import { data } from './data.js';
+import session from 'express-session';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/get', (req, res) => {
-    res.json(data);
-});
+// middleware
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionsSuccessStatus: 200
+}));
 
-app.get('/es', async (req, res) => {
-    const response = await fetch('http://localhost:9200');
-    const data = await response.json();
+app.use(session({
+    resave: true,
+    saveUninitialized: false, // this should be ok for authenticated users
+    secret: 'changeme',
+    cookie: {
+        httpOnly: true,
+        secure: false, // change for production
+        sameSite: true,
+        domain: 'localhost',
+        maxAge: 86400000 // 24 hours
+    }
+}));
+
+
+
+// endpoints
+app.get('/get', (req, res) => {
+    console.log(req.session.id);
     res.json(data);
 });
 
 app.get('/callback', async (req, res) => {
     const resFromGoogle = await fetch('https://oauth2.googleapis.com/token', {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
             code: req.query.code,
             client_id: process.env.CLIENT_ID,
@@ -33,15 +51,22 @@ app.get('/callback', async (req, res) => {
         })
     });
 
-    const {access_token, id_token} = (await resFromGoogle.json());
+    // TODO - set up a store so we can save these token with the session id
+    // - when authenticated user makes a request, we should be able to use their session id to get their tokens
+    // - access token can be used to check with Google if the user is still "valid".
+    // - id token can be parsed to provide info like name, email, etc.
+    const { access_token, id_token } = await resFromGoogle.json();
 
-    // TODO - redirect to UI with tokens
+    res.redirect(`http://localhost:3000`);
 })
 
 app.all('*', (req, res) => {
     res.status(404).json({ message: 'Not found' });
 });
 
+
+
+// server
 app.listen(port, () => {
     console.log(`~ Server is running at http://127.0.0.1:${port} ~`);
 });
