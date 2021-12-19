7ecb4c25245dad206e2cde7d4ce1f2d20f1333d4
Annmarie Switzer <aargiros@gmail.com>
2021-12-19 17:54:11 -0500

    chart WIP

--- react/src/lib/Gauge.js
+++ react/src/lib/Gauge.js
@@ -26,6 +26,18 @@ function Gauge({ selection }) {
         'var(--green)'
     ]);
 
+    const arcMap = {
+        calories: 'Calories',
+        water_ml: 'Water',
+        minutes: 'Time'
+    };
+
+    const trackData = [
+        {name: 'calories', value: userMaxCalories},
+        {name: 'water_ml', value: userMaxCalories},
+        {name: 'minutes', value: userMaxCalories},
+    ]
+
     const ref = useD3(
         (svg) => {
             setDimensions(() => ({
@@ -33,18 +45,36 @@ function Gauge({ selection }) {
                 height: window.innerHeight
             }));
 
-            const data = ['calories', 'water_ml', 'minutes'].map((name) => {
+            let data = ['calories', 'water_ml', 'minutes'].map((name) => {
+                // value is the reult of reduce or 0
                 let value =
                     selection
                         .map((s) => s[name])
                         .reduce((total, next) => total + next, 0) || 0;
 
+                // artificially decrease the scale of the `minutes` arc
                 if (name === 'minutes') {
-                    value = value * 60;
+                    value *= 60;
                 }
 
-                return { name, value };
+                const displayValue = (() => {
+                    switch (name) {
+                        case 'minutes':
+                            return `${value.toLocaleString() / 60} min`;
+                        case 'water_ml':
+                            return `${value.toLocaleString()} mL`;
+                        default:
+                            return value.toLocaleString();
+                    }
+                })();
+
+                return { name, value, displayName: arcMap[name], displayValue };
             });
+
+            // const noData = data.every((d) => d.value === 0);
+            // if (noData) {
+            //     data = data.map((d) => ({ ...d, value: userMaxCalories }));
+            // }
 
             const chartRadius = dimensions.height / 1.5;
             const arcMinRadius = 350;
@@ -72,6 +102,24 @@ function Gauge({ selection }) {
                 .startAngle(0)
                 .endAngle((d, i) => scale(d));
 
+            const tracks = d3
+                .select('.tracks')
+                .selectAll('path')
+                .data(trackData)
+                .join('path')
+                .attr('class', 'track')
+                .style('fill', 'var(--bg-lighter)')
+                // .on('mouseenter', (e, d) => setSelectedArc(d))
+                // .on('mouseleave', () => setSelectedArc(null));
+
+            tracks.transition()
+                .delay((d, i) => i * 0)
+                .duration(1000)
+                .attrTween('d', (d, i) => {
+                    const interpolate = d3.interpolate(0, d.value);
+                    return (t) => arc(interpolate(t), i);
+                });
+
             const arcs = d3
                 .select('.arcs')
                 .selectAll('path')
@@ -86,7 +134,7 @@ function Gauge({ selection }) {
                 .delay((d, i) => i * 0)
                 .duration(1000)
                 .attrTween('d', (d, i) => {
-                    let interpolate = d3.interpolate(0, d.value);
+                    const interpolate = d3.interpolate(0, d.value);
                     return (t) => arc(interpolate(t), i);
                 });
         },
@@ -106,16 +154,13 @@ function Gauge({ selection }) {
                         ${dimensions.height / 1.25}
                     )`}
                 >
+                    <g className="tracks"></g>
                     <g className="arcs"></g>
                 </g>
             </svg>
             <div id="center-text">
-                <span>{selectedArc?.name}</span>
-                <span>
-                    {selectedArc?.name === 'minutes'
-                        ? selectedArc?.value / 60
-                        : selectedArc?.value}
-                </span>
+                <span>{selectedArc?.displayName}</span>
+                <span>{selectedArc?.displayValue}</span>
             </div>
         </div>
     );
